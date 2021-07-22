a7feedeeda7515f647d37a474fb0daa9c901d81c
Lars Nørregaard <lnorregaard@nordija.com>
2021-07-22 15:30:37 +0200

    Fixed plugin naming in properties

--- src/test/groovy/com/_24i/VersionManagerPluginTest.groovy
+++ src/test/groovy/com/_24i/VersionManagerPluginTest.groovy
@@ -9,7 +9,7 @@ class VersionManagerPluginTest {
     @Test
     public void findVersionPluginAddsFindVersionTaskToProject() {
         Project project = ProjectBuilder.builder().build()
-        project.apply plugin: 'com.nordija.versionManager'
+        project.apply plugin: 'com.24i.gitVersion'
 
         assertTrue(project.tasks.version instanceof VersionManagerTask)
     }
