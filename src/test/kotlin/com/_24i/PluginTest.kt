2bb9c88728085de43c30471fd7df622ba70d34d6
milan jurkulák <mimoccc@gmail.com>
2025-04-08 13:47:08 +0200

    SPRINT-14393 gradle update, kts, kotlin - test fix

--- src/test/kotlin/com/_24i/PluginTest.kt
+++ src/test/kotlin/com/_24i/PluginTest.kt
@@ -10,21 +10,12 @@ class PluginTest {
     companion object {
         private const val TASK_NAME = "version"
         private const val PLUGIN_ID_GIT_VERSION = "com.24i.gitVersion"
-        private const val PLUGIN_ID_GIT_MAVEN_VERSION = "com.nordija.versionManager"
     }
 
     @Test
     fun testGitVersion() {
         val project: Project = ProjectBuilder.builder().build()
         project.plugins.apply(PLUGIN_ID_GIT_VERSION)
-        val hasTask = project.tasks.findByName(TASK_NAME) != null
-        assert(hasTask, { "No task $TASK_NAME found." })
-    }
-
-    @Test
-    fun testGitMavenVersion() {
-        val project = ProjectBuilder.builder().build()
-        project.getPlugins().apply(PLUGIN_ID_GIT_MAVEN_VERSION)
         val hasTask = project.tasks.findByName(TASK_NAME) != null
         assert(hasTask, { "No task $TASK_NAME found." })
     }
