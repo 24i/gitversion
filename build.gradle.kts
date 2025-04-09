dff3762734b19bf4998b67a15b307abaaa21f588
milan jurkulák <mimoccc@gmail.com>
2025-04-09 12:27:45 +0200

    SPRINT-14393 gradle update, kts, kotlin - vesioning fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ plugins {
     `maven-publish`
     `java-gradle-plugin`
     id("com.jfrog.artifactory") version ("4.33.1")
-    id("com.nordija.versionManager") version ("2.2.3")
 }
 kotlin {
     jvmToolchain(17)
